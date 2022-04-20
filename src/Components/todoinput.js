1c65ec9cfc0dcd154e6e9aa815ecc7cffcaafd63
hirunppal <hirunppal@gmail.com>
2022-04-20 09:58:09 +0700

    More Error Example

--- src/Components/todoinput.js
+++ src/Components/todoinput.js
@@ -30,11 +30,7 @@ function TodoInput(props) {
           }}
           color="success"
         >
-          <i
-            // onClick={() => props.createTodo(todoTextINP)}
-
-            className="fa-solid fa-plus"
-          ></i>
+          <i className="fa-solid fa-plus"></i>
         </Button>
 
         <Button onClick={() => SettodoTextINP("")} color="outline-secondary">
@@ -42,6 +38,66 @@ function TodoInput(props) {
         </Button>
       </div>
       {todoEror && <small className="text-danger"> {todoEror}</small>}
+      {/* example to more inp */}
+      <input
+        type="text"
+        className={`form-control ${todoEror ? "is-invalid" : ""}`}
+        placeholder="Enter new todo"
+        value={todoTextINP}
+        onChange={(event) => SettodoTextINP(event.target.value)}
+      />
+      <Button
+        onClick={() => {
+          if (!todoTextINP) {
+            SettodoEror("Title is Required");
+          } else {
+            props.createTodo(todoTextINP);
+            SettodoTextINP("");
+            SettodoEror("");
+          }
+        }}
+        color="success"
+      >
+        <i
+          // onClick={() => props.createTodo(todoTextINP)}
+
+          className="fa-solid fa-plus"
+        ></i>
+      </Button>
+      <Button onClick={() => SettodoTextINP("")} color="outline-secondary">
+        <i className="fa-solid fa-xmark"></i>
+      </Button>
+      {todoEror && <small className="text-danger"> {todoEror}</small>}
+      //example for more INP//
+      <div>
+        <input
+          type="text"
+          className={`form-control ${todoEror ? "is-invalid" : ""}`}
+          placeholder="Enter new todo"
+          value={todoTextINP}
+          onChange={(event) => SettodoTextINP(event.target.value)}
+        />
+        <Button
+          onClick={() => {
+            if (!todoTextINP) {
+              SettodoEror("Title is Required");
+            } else {
+              props.createTodo(todoTextINP);
+              SettodoTextINP("");
+              SettodoEror("");
+            }
+          }}
+          color="success"
+        >
+          <i className="fa-solid fa-plus"></i>
+        </Button>
+
+        <Button onClick={() => SettodoTextINP("")} color="outline-secondary">
+          <i className="fa-solid fa-xmark"></i>
+        </Button>
+
+        {todoEror && <small className="text-danger"> {todoEror}</small>}
+      </div>
     </>
   );
 }
